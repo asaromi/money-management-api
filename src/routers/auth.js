20e89ec5259f90f34bd2bf356074a0b22afca1a7
Asaromi <a.sabililromi@gmail.com>
2024-02-08 21:29:47 +0700

    fix: refactor codes

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,11 +1,19 @@
 const { Router } = require('express')
 const { getAuthUser, login, register } = require('../controllers/auth')
-const { validateSchema, handleResponse, authenticate } = require('../libs/middleware')
+const { validateSchema, handleResponse, authenticate } = require('../libs/middlewares')
 const { createSchema: createUserSchema } = require('../validators/user')
 const router = new Router()
 
-router.get('/', authenticate, getAuthUser, handleResponse)
-router.post('/register', validateSchema(createUserSchema), register, handleResponse)
+router.get('/',
+	authenticate,
+	getAuthUser,
+	handleResponse
+)
+router.post('/register',
+	validateSchema(createUserSchema),
+	register,
+	handleResponse
+)
 router.post('/login', login, handleResponse)
 
 module.exports = router
