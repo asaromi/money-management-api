a0b3a3bbac4bb06a5652edfee154ffa2f15e8209
Asaromi <a.sabililromi@gmail.com>
2024-02-08 18:38:44 +0700

    feat: add user model & migration

---
+++ src/databases/migrations/20240208014617-create-user.js
@@ -0,0 +1,56 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		const transaction = await queryInterface.sequelize.transaction()
+		try {
+			await queryInterface.createTable('users', {
+				id: {
+					allowNull: false,
+					primaryKey: true,
+					type: Sequelize.STRING(26),
+				},
+				email: {
+					allowNull: false,
+					type: Sequelize.STRING,
+				},
+				password: {
+					allowNull: false,
+					type: Sequelize.TEXT,
+				},
+				name: {
+					allowNull: false,
+					type: Sequelize.STRING,
+				},
+				createdAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+				},
+				updatedAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+				},
+				deletedAt: {
+					allowNull: true,
+					type: Sequelize.DATE,
+				},
+			}, { transaction })
+
+			await transaction.commit()
+		} catch (error) {
+			await transaction.rollback()
+			throw error
+		}
+	},
+	async down(queryInterface, Sequelize) {
+		const transaction = await queryInterface.sequelize.transaction()
+
+		try {
+			await queryInterface.dropTable('users')
+			await transaction.commit()
+		} catch (error) {
+			await transaction.rollback()
+			throw error
+		}
+	},
+}
---
+++ src/databases/models/user.js
@@ -0,0 +1,50 @@
+'use strict'
+const { DataTypes: DT, Model } = require('sequelize')
+const { generateId } = require('../../libs/ulid')
+
+module.exports = (sequelize, DataTypes = DT) => {
+	class User extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			this.hasMany(models.Wallet, {
+				foreignKey: 'userId',
+				as: 'wallets'
+			})
+		}
+	}
+
+	User.init({
+		id: {
+			allowNull: false,
+			primaryKey: true,
+			type: DataTypes.STRING(26),
+			defaultValue: generateId(),
+		},
+		email: {
+			allowNull: false,
+			type: DataTypes.STRING,
+		},
+		password: {
+			allowNull: false,
+			type: DataTypes.TEXT,
+		},
+		name: {
+			allowNull: false,
+			type: DataTypes.STRING,
+		},
+	}, {
+		defaultScope: {
+			attributes: { exclude: ['deletedAt'] }
+		},
+		modelName: 'User',
+		paranoid: true,
+		sequelize,
+		tableName: 'users',
+	})
+	return User
+}
